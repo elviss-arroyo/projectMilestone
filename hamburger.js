755ffc94d29c06f2b3da389ce7cb9aaad4b2c31f
elviss-arroyo <fabhytt@gmail.com>
2025-12-08 19:53:17 -0500

    Update hamburger.js

--- hamburger.js
+++ hamburger.js
@@ -1,5 +1,5 @@
 // Toggle the navigation menu on mobile/tablet
 function toggleMenu() {
-  const menu = document.getElementById("myLinks");
-  menu.classList.toggle("show");
+  const menu = document.getElementById("myLinks"); // looks for menu on page
+  menu.classList.toggle("show"); // turns menu on or off 
 }
